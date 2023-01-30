72782572e4905ac5f91fd3e82024110a7afd3cce
Saiyanprincess11 <pth3035@rit.edu>
2023-01-30 14:51:00 -0500

    Started Assigment

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+const mediaHandler = require('./mediaResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// Starts server and listens for traffic
+const onRequest = (request, response) => {
+  console.log(request.url);
+
+  // Handles different URLs
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/page2':
+      htmlHandler.getIndex(request, response); 
+      break; 
+    case '/page3':
+      htmlHandler.getIndex(request, response); 
+      break; 
+    case '/party.mp4':
+      mediaHandler.getParty(request, response);
+      break;
+    case '/bird.mp4':
+      mediaHandler.getBird(request, response); 
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
